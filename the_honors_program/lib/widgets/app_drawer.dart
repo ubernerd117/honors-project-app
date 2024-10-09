3c4e5ce38e3e8a3257c1ffc263ae57831a9810d0
Sourab <sourab@MacBookAir.lan>
2024-10-09 10:09:01 -0400

    new icons

--- the_honors_program/lib/widgets/app_drawer.dart
+++ the_honors_program/lib/widgets/app_drawer.dart
@@ -7,72 +7,82 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
+      child: Column(
+        children: [
           const DrawerHeader(
             decoration: BoxDecoration(
               color: Colors.deepOrange,
             ),
-            child: Text(
-              'Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+            child: Center(
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
               ),
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/home');
-            },
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.timeline),
+                  title: const Text('My Timeline'),
+                  onTap: () {
+                    // Navigate to Profile Page
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.school),
+                  title: const Text('What is the Honors Project?'),
+                  onTap: () {
+                    // Navigate to Project Intro page
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.library_books),
+                  title: const Text('Honors Project Resources'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(
+                        context, '/projectResources');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.supervisor_account),
+                  title: const Text('Advisor Guidelines'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/adv_guidelines');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.question_answer),
+                  title: const Text('Students FAQ'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/student_faq');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.history_edu),
+                  title: const Text('Past Projects'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/pastProjects');
+                  },
+                ),
+              ],
+            ),
           ),
+          const Divider(),
           ListTile(
             leading: const Icon(Icons.account_circle),
-            title: const Text('My Timeline'),
-            onTap: () {
-              // Navigate to Profile Page
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('What is the Honors Project?'),
-            onTap: () {
-              // Navigate to Project Intro page
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Honors Project Resouces'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/projectResources');
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Advisor Guidelines'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/adv_guidelines');
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Students FAQ'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/student_faq');
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Past Projects'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/pastProjects');
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
             title: const Text('Profile'),
             onTap: () {
               Navigator.pushReplacementNamed(context, '/profile');
